5d77ba68bf7c892eb48f5e9a845498dedb031b68
Reda Damene <90632620+RedaDamene@users.noreply.github.com>
2023-08-08 15:46:31 +0200

    Feat: TP3

---
+++ src/Controller/WishController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/wish', name: 'wish_')]
+class WishController extends AbstractController
+{
+    #[Route('/list', name: 'list')]
+    public function list(): Response
+    {
+        return $this->render(
+            'wish/list.html.twig'
+        );
+    }
+    #[Route('/detail/{id}', name: 'detail', requirements: ["id" => "\d+"])]
+    public function detail(int $id): Response
+    {
+        return $this->render(
+            'wish/detail.html.twig',
+            compact('id')
+        );
+    }
+}
