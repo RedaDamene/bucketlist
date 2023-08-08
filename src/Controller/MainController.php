4be6cdf9e625ee79204c43766c8c4ad2a3244377
Reda Damene <90632620+RedaDamene@users.noreply.github.com>
2023-08-08 14:18:56 +0200

    Feat: TP3

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -15,4 +15,18 @@ class MainController extends AbstractController
             'main/home.html.twig'
         );
     }
+
+    #[Route('/aboutus', name: 'main_aboutus')]
+    public function aboutus(): Response
+    {
+
+        $path = __DIR__ . '..\..\data\team.json';
+        $jsonTeam = file_get_contents($path);
+        $teams = json_decode($jsonTeam);
+
+        return $this->render(
+            'main/aboutus.html.twig',
+            compact('teams')
+        );
+    }
 }
